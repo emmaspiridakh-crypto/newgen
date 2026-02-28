25a632137cf374865300df47b3265c92651e427c
emma sp <emmaspiridakh@gmail.com>
2026-02-28 17:44:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -744,11 +744,9 @@ async def on_ready():
 # START BOT
 # ================================
 
-from keep_alive import keep_alive
-keep_alive()
-
 if __name__ == "__main__":
     bot.run(TOKEN)
+
 
 
 
